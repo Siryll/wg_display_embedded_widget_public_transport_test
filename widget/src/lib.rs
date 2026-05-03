25b90ad32871617b46825b74105dabf4e6dee86f
Siryll <50954074+Siryll@users.noreply.github.com>
2026-05-03 13:18:51 +0200

    implement widget that hopefully is bigger than 508kb

--- widget/src/lib.rs
+++ widget/src/lib.rs
@@ -1,20 +1,61 @@
-use schemars::{schema_for, JsonSchema};
-use serde::Deserialize;
-use serde_json::{Error, Value};
+use serde_json::Error;
 
-use widget::widget::{http, logging};
+use schemars::JsonSchema;
+use serde::Deserialize;
+use time::format_description;
+use time::OffsetDateTime;
+use time_humanize::Accuracy;
+use time_humanize::HumanTime;
+use time_humanize::Tense;
+
+use widget::widget::{clocks, http};
 
 wit_bindgen::generate!({
     path: "../wg_display_widget_wit/wit",
     world: "widget"
 });
 
-const WIDGET_NAME: &str = "ESP32 Widget Template";
+#[derive(Deserialize)]
+struct FromData {
+    #[serde(with = "time::serde::iso8601")]
+    departure: OffsetDateTime,
+}
+
+#[derive(Deserialize)]
+struct FromMetaData {
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct ToMetaData {
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct ConnectionData {
+    from: FromData,
+}
+
+#[derive(Deserialize)]
+struct PublicTransportData {
+    connections: Vec<ConnectionData>,
+    from: FromMetaData,
+    to: ToMetaData,
+}
+
+#[derive(JsonSchema, Deserialize)]
+struct Connection {
+    from_station: String,
+    to_station: String,
+    num_connections: u8,
+}
 
 #[derive(JsonSchema, Deserialize)]
 struct WidgetConfig {
-    city: String,
+    connections: Vec<Connection>,
 }
+
+const WIDGET_NAME: &str = "Public Transport";
 
 struct MyWidget;
 
@@ -24,62 +65,119 @@ impl Guest for MyWidget {
     }
 
     fn run(context: WidgetContext) -> WidgetResult {
-        // Widgets can log to the console
-        logging::log(logging::Level::Info, WIDGET_NAME, "Widget run started");
-
-        // Widgets can handle the case where no config is provided
         if "{}" == context.config {
             return WidgetResult {
                 data: "No config provided".into(),
             };
         }
 
-        // Widgets can parse their config with ease using serde
         let config: WidgetConfig =
             serde_json::from_str(&context.config).expect("Failed to parse config");
 
-        // Widgets can make network requests
-        let response = http::request(
-            http::Method::Get,
-            format!(
-                "https://aareguru.existenz.ch/v2018/today?city={}",
-                config.city
-            )
-            .as_str(),
-            None,
-        );
-        let Ok(response) = response else {
-            return WidgetResult {
-                data: "Failed to make network request".into(),
-            };
-        };
+        let text_buffer = config.connections.iter()
+            .map(|connection| {
+                match MyWidget::fetch_connection(connection) {
+                    Ok(content) => content,
+                    Err(error) => error.data
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
 
-        if 200 != response.status {
-            return WidgetResult {
-                data: format!("Response status != 200: {}", response.status),
-            };
-        }
-
-        let data: Result<Value, Error> = serde_json::from_slice(response.bytes.as_slice());
-        let result = match data {
-            Ok(data) => format!("Aare Temperature in {}: {} C", config.city, data["aare"]),
-            Err(_) => "Response from AareGuru could not be parsed".into(),
-        };
-
-        WidgetResult { data: result }
+        WidgetResult { data: text_buffer }
     }
 
-    fn get_config_schema() -> String  {
-        let schema = schema_for!(WidgetConfig);
+    fn get_config_schema() -> String {
+        let mut settings = schemars::gen::SchemaSettings::default();
+        settings.inline_subschemas = true;
+        let schema = settings.into_generator().into_root_schema_for::<WidgetConfig>();
         serde_json::to_string_pretty(&schema).unwrap()
     }
 
     fn get_version() -> String {
-        "0.1.0-interface-test".into()
+        "1.0.0".into()
     }
 
     fn get_run_update_cycle_seconds() -> u32 {
-        10
+        90
+    }
+}
+
+impl MyWidget {
+    pub fn now() -> OffsetDateTime {
+        let now = clocks::now();
+        OffsetDateTime::from_unix_timestamp(now.seconds as i64).unwrap()
+    }
+
+    pub fn fetch_connection(connection: &Connection) -> Result<String, WidgetResult> {
+        let url = format!(
+                "http://transport.opendata.ch/v1/connections?from={}&to={}&limit=16",
+                urlencoding::encode(connection.from_station.as_str()),
+                urlencoding::encode(connection.to_station.as_str()),
+            );
+
+            let response = http::request(http::Method::Get, url.as_str(), None);
+            let Ok(response) = response else {
+                return Err(WidgetResult {
+                    data: "Failed to make network request".into(),
+                });
+            };
+
+            if 200 != response.status {
+                return Err(WidgetResult {
+                    data: format!("Response status != 200: {}", response.status),
+                });
+            }
+
+            let data: Result<PublicTransportData, Error> =
+                serde_json::from_slice(response.bytes.as_slice());
+            if let Err(error) = data {
+                return Err(WidgetResult {
+                    data: format!("Failed to parse response: {:?}", error),
+                });
+            };
+            let data = data.unwrap();
+            Ok(MyWidget::get_departure_string(&data, connection.num_connections as usize))
+
+    }
+
+    pub fn get_departure_string(data: &PublicTransportData, num_departures: usize) -> String {
+        let mut content = format!("{} -> {}", data.from.name, data.to.name);
+
+        if data.connections.is_empty() {
+            content += "\nNo departures";
+            return content;
+        }
+
+        let connections = data
+            .connections
+            .iter()
+            .filter(|connection| (connection.from.departure - MyWidget::now()).is_positive())
+            .take(num_departures);
+
+        for connection in connections {
+            let departure = connection.from.departure;
+            content += &format!(
+                "\n{} ({})",
+                MyWidget::format_departure_offset(departure),
+                MyWidget::format_departure_time(departure)
+            )
+            .to_string();
+        }
+        content
+    }
+
+    pub fn format_departure_time(departure: OffsetDateTime) -> String {
+        let format = format_description::parse("[hour]:[minute]").unwrap();
+        match departure.format(&format) {
+            Ok(departure) => departure,
+            Err(_) => "Could not format departure".to_string(),
+        }
+    }
+
+    pub fn format_departure_offset(departure: OffsetDateTime) -> String {
+        let departure_offset = departure - MyWidget::now();
+        HumanTime::from(departure_offset.unsigned_abs()).to_text_en(Accuracy::Rough, Tense::Future)
     }
 }
 
